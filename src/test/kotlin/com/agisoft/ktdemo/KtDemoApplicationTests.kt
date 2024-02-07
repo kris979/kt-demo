668dbcd9fc9b2814fd12058531d882b1e7308fd1
kris979 <kris979@gmail.com>
2024-02-07 14:17:50 +0000

    initial commit

---
+++ src/test/kotlin/com/agisoft/ktdemo/KtDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.agisoft.ktdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
