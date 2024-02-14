a9826743704153219a0626db1d5b23a7bf415be8
kris979 <kris979@gmail.com>
2024-02-14 14:48:47 +0000

    editing templates.

---
+++ src/main/kotlin/com/agisoft/ktdemo/HtmlController.kt
@@ -0,0 +1,39 @@
+package com.agisoft.ktdemo
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController(private val repository: ArticleRepository,
+                     private val properties: BlogProperties) {
+
+    @GetMapping("/")
+    fun blog(model: Model): String {
+        model["title"] = properties.title
+        model["banner"] = properties.banner
+        model["articles"] = repository.findAllByOrderByAddedAtDesc().map { it.render() }
+        model["footer"] = "Agisoft Limited. All rights reserved. "
+        return "blog"
+    }
+
+    fun Article.render() = RenderedArticle(
+        slug,
+        title,
+        headline,
+        content,
+        author,
+        addedAt.format()
+    )
+
+    data class RenderedArticle(
+        val slug: String,
+        val title: String,
+        val headline: String,
+        val content: String,
+        val author: User,
+        val addedAt: String
+    )
+
+}
--- src/main/kotlin/com/agisoft/ktdemo/KtDemoApplication.kt
+++ src/main/kotlin/com/agisoft/ktdemo/KtDemoApplication.kt
@@ -2,9 +2,11 @@ package com.agisoft.ktdemo
 
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(BlogProperties::class)
 class KtDemoApplication
 
 fun main(args: Array<String>) {
